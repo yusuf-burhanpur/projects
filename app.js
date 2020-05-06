fafb8085d2b67311faae2dfe2cc3f7c42677a0a0
yusufburhanpur1@gmail.com <yusufburhanpur1@gmail.com>
2020-05-07 00:49:46 +0530

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "final.html");
+    res.sendFile(__dirname + "/public/final.html");
 });
 
 
